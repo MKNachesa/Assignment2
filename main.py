8c127a606dcd6874f1e8a15b4e2ffc92cf21d671
MayaKN <maya.nachesa@gmail.com>
2022-02-02 15:08:49 +0100

    Uploaded linear_search, bubble_sort, get_random, main

---
+++ main.py
@@ -0,0 +1,16 @@
+from bubble_sort import bubble_sort
+from linear_search import linear_search
+from get_random import get_random
+import time
+import random
+
+
+if __name__ == "__main__":
+    lenlist = 20
+    lst = get_random(lenlist)
+    search_start = time.time()
+    target = random.choice([lst[random.randint(0, lenlist-1)], 100001])
+    search_stop = time.time()
+    search_running_time = search_stop - search_start
+    print(linear_search(lst, target))
+    print(f"{search_running_time: .20f}")
